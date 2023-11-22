6ea82ce3618106059e0a9123b1c03e78a4954f30
Benjamin Haase <benjaminthaase@protonmail.com>
2023-11-22 11:53:25 -0800

    Refactors Event enum in library into two separate structs, starts the redesign of the broker/chatroom broker as well

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,6 @@
 use std::fmt::Debug;
+use std::sync::Arc;
+use async_std::io::{self, Read, ReadExt, Write, WriteExt};
 use async_std::channel::{self, Sender, Receiver};
 use async_std::net::ToSocketAddrs;
 use async_std::net::{TcpListener, TcpStream};
@@ -7,7 +9,7 @@ use futures::{FutureExt, StreamExt};
 use uuid::Uuid;
 
 use crate::server_error::ServerError;
-use rusty_chat::Event;
+use rusty_chat::{*, Event, Null};
 
 mod chatroom_task;
 mod server_error;
@@ -22,7 +24,7 @@ async fn accept_loop(server_addrs: impl ToSocketAddrs + Clone + Debug, channel_b
     let (broker_sender, broker_receiver) = channel::bounded::<Event>(channel_buf_size);
 
     // spawn broker task
-    task::spawn(broker(broker_receiver));
+    task::spawn(broker(broker_sender.clone(),broker_receiver));
 
     while let Some(stream) = listener.incoming().next().await {
         let stream = stream.map_err(|_| ServerError::ConnectionFailed)?;
@@ -33,14 +35,52 @@ async fn accept_loop(server_addrs: impl ToSocketAddrs + Clone + Debug, channel_b
     Ok(())
 }
 
-async fn handle_connection(broker_sender: Sender<Event>, client_stream: TcpStream) -> Result<(), ServerError> {
-    let client_id = Uuid::new_v4();
+async fn handle_connection(main_broker_sender: Sender<Event>, client_stream: TcpStream) -> Result<(), ServerError> {
+    let client_stream = Arc::new(client_stream);
+    let mut client_reader = &*client_stream;
 
-    todo!();
+    // Id for the client
+    let peer_id = Uuid::new_v4();
+
+    // channel for synchronizing graceful shutdown with the writer task
+    let (_client_shutdown_sender, client_shutdown_receiver) = channel::unbounded::<Null>();
+
+    // channel for orchestrating the connection between main broker, chatroom broker and client
+    // read/write tasks
+    let (chatroom_broker_sender, chatroom_broker_receiver) = channel::unbounded::<AsyncStdSender<Event>>();
+
+    // new client event
+    let event = Event::NewClient {
+        peer_id,
+        stream: client_stream.clone(),
+        shutdown: client_shutdown_receiver,
+        chatroom_connection: chatroom_broker_sender
+    };
+
+    // for sending message events when the client joins a new chatroom.
+    let mut chatroom_sender: Option<AsyncStdSender<Event>> = None;
+
+    // send a new client event to the main broker, should not be disconnected
+    // TODO: add logging/tracing
+    main_broker_sender.send(event)
+        .await
+        .expect("broker should be connected");
+
+    let mut frame_tag = [0u8; 5];
+
+    while let Ok(_) = client_reader.read_exact(&mut frame_tag) {
+        // Attempt to parse the frame from the client
+        todo!()
+    }
+
+
+    todo!()
 }
 
-async fn broker(event_receiver: Receiver<Event>) -> Result<(), ServerError> {
-    todo!()
+async fn client_write_loop() -> Result<(), ServerError> {todo!()}
+
+async fn broker(_event_sender: Sender<Event>, event_receiver: Receiver<Event>) -> Result<(), ServerError> {
+
 }
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 use uuid::Uuid;
 
 use std::collections::HashMap;
+use std::sync::Arc;
 use async_std::net;
 pub use async_std::channel::{Receiver as AsyncStdReceiver, Sender as AsyncStdSender};
 pub use tokio::sync::broadcast::{Sender as TokioBroadcastSender, Receiver as TokioBroadcastReceiver};
@@ -22,69 +23,131 @@ pub struct Chatroom {
 
 }
 
-// TODO: implement parsing to and from bytes for message events
-#[derive(Clone)]
+#[derive(Debug)]
 pub enum Event {
-    Quit,
+    Quit {peer_id: Uuid},
     Join {
         chatroom_name: String,
+        peer_id: Uuid
     },
     Create {
         chatroom_name: String,
+        peer_id: Uuid
+    },
+    Username {
+        new_username: String,
+        peer_id: Uuid
     },
     NewClient {
-        username: String,
-        stream: net::TcpStream,
-    },
-    UpdateUsername {
-        new_username: String,
+        stream: Arc<net::TcpStream>,
+        shutdown: AsyncStdReceiver<Null>,
+        chatroom_connection: AsyncStdSender<AsyncStdSender<Event>>,
         peer_id: Uuid,
     }
 }
 
-impl Event {
-    
-    fn parse_length(tag: &mut EventTag, length: u32) {
-        for i in 1..tag.len() {
-            tag[i] ^= ((length >> ((i - 1) * 8)) & 0xff) as u8;
+
+// TODO: implement parsing to and from bytes for message events
+#[derive(Clone)]
+pub enum Frame {
+    Quit,
+
+    Join {
+        chatroom_name: String,
+    },
+
+    Create {
+        chatroom_name: String,
+    },
+
+    Username {
+        new_username: String,
+    },
+
+    Message {
+        message: String,
+    }
+}
+
+impl Frame {
+    fn serialize_len(tag: &mut FrameEncodeTag, idx: usize, length: u32) {
+        for i in idx ..idx + 4 {
+            tag[i] ^= ((length >> ((i - idx) * 8)) & 0xff) as u8;
+        }
+    }
+
+    fn deserialize_len(tag: &FrameEncodeTag, idx: usize, length: &mut u32) {
+        for i in idx .. idx + 4 {
+            *length ^= (tag[i] as u32) << ((i - idx) * 8);
         }
     }
 }
 
-pub type EventTag = [u8; 5];
+pub type FrameEncodeTag = [u8; 5];
 
-impl SerializationTag for EventTag {}
+impl SerializationTag for FrameEncodeTag {}
 
-pub type EventTvlTag = (u8, u32);
+pub type FrameDecodeTag = (u8, u32);
 
-impl DeserializationTag for EventTvlTag {}
+impl DeserializationTag for FrameDecodeTag {}
 
-impl SerAsBytes for Event {
-    type Tag = EventTag;
+impl SerAsBytes for Frame {
+    type Tag = FrameEncodeTag;
 
     fn serialize(&self) -> Self::SerializationTag {
         let mut tag = [0u8; 5];
+
         match self {
-            Event::Quit => {
-                tag[0] ^= 1;
-            }
-            Event::Create {chatroom_name} => {
+            Frame::Quit => tag[0] ^= 1,
+            Frame::Join {chatroom_name} => {
                 tag[0] ^= 1 << 1;
-                Event::parse_length(&mut tag, chatroom_name.len() as u32);
+                Frame::serialize_len(&mut tag, 1, chatroom_name.len() as u32);
             }
-            Event::Join {chatroom_name} => {
+            Frame::Create {chatroom_name} => {
                 tag[0] ^= 1 << 2;
-                Event::parse_length(&mut tag, chatroom_name.len() as u32);
+                Frame::serialize_len(&mut tag, 1, chatroom_name.len() as u32);
+            }
+            Frame::Username {new_username} => {
+                tag[0] ^= 1 << 3;
+                Frame::serialize_len(&mut tag, 1,new_username.len() as u32);
+            }
+            Frame::Message { message} => {
+                tag[0] ^= 1 << 4;
+                Frame::serialize_len(&mut tag, 1, message.len() as u32);
             }
         }
-        
+
         tag
     }
 }
 
-// impl DeSerAsBytes for Event {
-//
-// }
+impl DeSerAsBytes for Frame {
+    type TvlTag = FrameDecodeTag;
+
+    fn deserialize(tag: Self::Tag) -> Self::TvlTag {
+        if tag[0] & 1 != 0 {
+            (1, 0)
+        } else if tag[0] & 2 != 0 {
+            let mut chatroom_name_len = 0u32;
+            Frame::deserialize_len(&tag, 1, &mut chatroom_name_len);
+            (2, chatroom_name_len)
+        } else if tag[0] & 4 != 0 {
+            let mut chatroom_name_len = 0u32;
+            Frame::deserialize_len(&tag, 1, &mut chatroom_name_len);
+            (4, chatroom_name_len)
+        } else if tag[0] & 8 != 0 {
+            let mut username_len = 0;
+            Frame::deserialize_len(&tag,1, &mut username_len);
+            (8, username_len, 0)
+        } else if tag[0] & 16 != 0 {
+            let mut message_len = 0;
+            Frame::deserialize_len(&tag, 1, &mut message_len);
+            (16, message_len)
+        } else {
+            panic!("invalid type byte detected, unable to deserialize 'Frame' tag")
+        }
+    }
+}
 
 
 pub struct Client {
